be7c54b7f71d039b248da60c283c0cf5a51afcfd
Enoch Chau <enoch965@gmail.com>
2020-12-28 13:44:22 -0800

    fixing numlock backlight

--- keyboards/dropout/dropout.c
+++ keyboards/dropout/dropout.c
@@ -1,14 +1,7 @@
 #include "dropout.h"
 
-//by default, the backlight implementation is used for the numlock so that the led brightness can be controlled via pwm.
-//enable this to use the default num lock indicator implementation
-//#define DEFAULT_NUMLOCK 
-#ifdef DEFAULT_NUMLOCK
-#   define LED_NUM_LOCK_PIN B5
-#endif
-
 void led_init_kb(void){
-#ifdef DEFAULT_NUMLOCK
+#ifndef BACKLIGHT_NUMLOCK
   // numlock led is wired like so:
   // 5V -> led -> pin
   setPinOutput(LED_NUM_LOCK_PIN); // numlock led
@@ -19,12 +12,13 @@ void led_init_kb(void){
 bool led_update_kb(led_t led_state) {
   bool res = led_update_user(led_state);
   if(res) {
-#ifdef DEFAULT_NUMLOCK
+#ifndef BACKLIGHT_NUMLOCK
     writePin(LED_NUM_LOCK_PIN, led_state.num_lock)
 #endif
-#ifndef DEFAULT_NUMLOCK
+
+#ifdef BACKLIGHT_NUMLOCK
     //enable or disable the backlight based on num lock state
-    if (led_state.num_lock){
+    if (!led_state.num_lock){
       backlight_enable();
     } else {
       backlight_disable();
